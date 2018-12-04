4d18b994895924ef20a160b5d6dd1da7164cb09d
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2018-12-04 18:59:08 +0100

    h2 db init

--- src/main/java/pl/kkwiatkowski/loan/dto/Loan.java
+++ src/main/java/pl/kkwiatkowski/loan/dto/Loan.java
@@ -5,15 +5,27 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.joda.time.DateTime;
 
+import javax.persistence.*;
 import java.math.BigDecimal;
 
 @Getter
 @Setter
 @NoArgsConstructor
+@Entity
+@Table(name = "Loan")
 public class Loan {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "sequenceGenerator")
+    @SequenceGenerator(name = "sequenceGenerator")
     private String loanId;
+
+    @Column(name = "loan_amount")
     private BigDecimal loanAmount;
+    @Column(name = "loan_term")
     private DateTime loanTerm;
+    @Column(name = "loan_issued_date")
     private DateTime loanIssuedDate;
+    @Column(name = "last_extend_date")
     private DateTime lastExtendDate;
 }
