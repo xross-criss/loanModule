8374fef09d5968ceb64613ecec07670f15422019
Krzysztof Kwiatkowski <krzysztof.kwiatkowski@morecraft.pl>
2018-12-04 04:47:32 +0100

    logic implemented

--- src/main/java/pl/kkwiatkowski/loan/constants/Constants.java
+++ src/main/java/pl/kkwiatkowski/loan/constants/Constants.java
@@ -1,6 +1,8 @@
 package pl.kkwiatkowski.loan.constants;
 
 import org.joda.time.Duration;
+import org.joda.time.Hours;
+import org.joda.time.Minutes;
 
 import java.math.BigDecimal;
 
@@ -9,4 +11,9 @@ public class Constants {
     public static final BigDecimal maxAmount = BigDecimal.valueOf(50000.0);
     public static final Duration minLoanDuration = Duration.standardDays(60);
     public static final Duration maxLoanDuration = Duration.standardDays(730);
+    public static final Hours systemOffStartPeriodHours = Hours.parseHours("00");
+    public static final Minutes systemOffStartPeriodMinutes= Minutes.parseMinutes("00");
+    public static final Hours systemOffEndPeriodHours = Hours.parseHours("06");
+    public static final Minutes systemOffEndPeriodMinutes = Minutes.parseMinutes("00");
+    public static final BigDecimal interestPercentage = BigDecimal.valueOf(10/100);
 }
--- src/main/java/pl/kkwiatkowski/loan/dto/LoanResponse.java
+++ src/main/java/pl/kkwiatkowski/loan/dto/LoanResponse.java
@@ -1,42 +1,16 @@
 package pl.kkwiatkowski.loan.dto;
 
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 import org.joda.time.DateTime;
 
 import java.math.BigDecimal;
 
-@Getter
-@Setter
 @NoArgsConstructor
 @ToString
+@Builder
 public class LoanResponse {
     private BigDecimal loanAmount;
     private BigDecimal repaymentAmount;
     private DateTime loanActionDate;
     private DateTime loanTerm;
 }
-
-
-
-/*
-    REST interface
-define max/min amount and max/min term (days)
-        no installments
-        available operations are:
-        apply_for_loan (term, amount)
-        if application is not within amount/term range reject application
-        if application is between 00:00 and 06:00 and max amount is asked then reject application
-        issued loan has 10% of principal (not 10% per year)
-        extend loan - (extension term is preconfigured. Upon extension the due date is changed, original due date + term)
-        junit tests
-        integration success path scenario test (loan issued)
-        post the solution to a public either github or bitbucket repo
-        no GUI
-        no authorization
-        no users
-        SOLID etc
-
-*/
