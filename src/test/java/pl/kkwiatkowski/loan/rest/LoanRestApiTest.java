b8dbd572d644c9f89409abcc6f73f3662b76188b
Krzysztof Kwiatkowski <kkwiatkowski@bluesoft.net.pl>
2018-12-04 14:26:19 +0100

    Simple Tests

--- src/test/java/pl/kkwiatkowski/loan/rest/LoanRestApiTest.java
+++ src/test/java/pl/kkwiatkowski/loan/rest/LoanRestApiTest.java
@@ -16,6 +16,8 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import pl.kkwiatkowski.loan.Application;
 import pl.kkwiatkowski.loan.common.Util;
+import pl.kkwiatkowski.loan.constants.Constants;
+import pl.kkwiatkowski.loan.dto.Loan;
 import pl.kkwiatkowski.loan.dto.LoanRequest;
 import pl.kkwiatkowski.loan.dto.LoanResponse;
 
@@ -38,7 +40,7 @@ public class LoanRestApiTest {
     private static final String REST_ROOT = "/api/loan";
 
     @Test
-    public void applyForLoanSuccess() throws Exception {
+    public void applyForLoanSuccessfully() throws Exception {
         LoanRequest request = new LoanRequest();
         BigDecimal askedAmount = BigDecimal.valueOf(12000);
         Duration askedDuration = Duration.standardDays(120);
@@ -59,6 +61,93 @@ public class LoanRestApiTest {
         assertNotNull(response);
         assertEquals(askedDate, response.getLoanTerm());
         assertEquals(askedAmount, response.getLoanAmount());
+        assertEquals(askedAmount.multiply(Constants.interestPercentage), response.getRepaymentAmount());
+    }
+
+    @Test
+    public void applyForLoanFailedWithAmountTooSmall() throws Exception {
+        LoanRequest request = new LoanRequest();
+        BigDecimal askedAmount = BigDecimal.valueOf(120);
+        Duration askedDuration = Duration.standardDays(120);
+
+        request.setIssuedAmount(askedAmount);
+        request.setIssuedDuration(askedDuration);
+
+        mockMvc.perform(post(getUri("/apply_for_loan"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Util.asJsonString(request)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void applyForLoanFailedWithTermTooShort() throws Exception {
+        LoanRequest request = new LoanRequest();
+        BigDecimal askedAmount = BigDecimal.valueOf(12000);
+        Duration askedDuration = Duration.standardDays(12);
+
+        request.setIssuedAmount(askedAmount);
+        request.setIssuedDuration(askedDuration);
+
+        mockMvc.perform(post(getUri("/apply_for_loan"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Util.asJsonString(request)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void applyForLoanFailedWithAmountTooBig() throws Exception {
+        LoanRequest request = new LoanRequest();
+        BigDecimal askedAmount = BigDecimal.valueOf(120000);
+        Duration askedDuration = Duration.standardDays(120);
+
+        request.setIssuedAmount(askedAmount);
+        request.setIssuedDuration(askedDuration);
+
+        mockMvc.perform(post(getUri("/apply_for_loan"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Util.asJsonString(request)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void applyForLoanFailedWithTermTooLong() throws Exception {
+        LoanRequest request = new LoanRequest();
+        BigDecimal askedAmount = BigDecimal.valueOf(12000);
+        Duration askedDuration = Duration.standardDays(120000);
+
+        request.setIssuedAmount(askedAmount);
+        request.setIssuedDuration(askedDuration);
+
+        mockMvc.perform(post(getUri("/apply_for_loan"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Util.asJsonString(request)))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void extendLoanTerm() throws Exception {
+        Loan request = new Loan();
+        BigDecimal askedAmount = BigDecimal.valueOf(12000);
+        Duration askedDuration = Duration.standardDays(60);
+
+        request.setLoanId("123456ASD");
+        request.setLoanAmount(askedAmount);
+        request.setLoanIssuedDate(DateTime.now().minus(askedDuration));
+        request.setLoanTerm(DateTime.now().plus(askedDuration));
+
+        MvcResult result = mockMvc.perform(post(getUri("/apply_for_loan"))
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(Util.asJsonString(request)))
+                .andExpect(status().isOk()).andReturn();
+
+        ObjectMapper mapper = new ObjectMapper();
+        LoanResponse response = mapper.readValue(result.getResponse().getContentAsString(), new TypeReference<LoanResponse>() {
+        });
+
+        assertNotNull(response);
+        assertEquals(DateTime.now().plus(askedDuration).plus(Constants.durationOfExtension), response.getLoanTerm());
+        assertEquals(DateTime.now().minus(askedDuration, response.getLoanIssuedDate()));
+        assertNotNull(response.getLastExtendDate());
     }
 
     private String getUri(String uri) {
